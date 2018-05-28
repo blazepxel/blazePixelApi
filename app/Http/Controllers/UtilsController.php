2e5f2a619f479edebfb44eb06ad02094bae52efc
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2018-05-28 07:43:11 -0500

    Added sendgrid

--- app/Http/Controllers/UtilsController.php
+++ app/Http/Controllers/UtilsController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Mail;
+
 
 class UtilsController extends Controller
 {
     function contact (Request $request) {
-      return $request;
+      $contact = $request->getContent();
+      // Mail::send('mails.contact', ['name' => $request->name],
+      //   function($message) use ($contact){
+      //   $message->from('abelorihuela@wtc-talent.com', 'Encuesta ');
+      //   $message->to('abelorihuelamendoza@hotmail.com', 'Buenas Tardes ...')->subject('Contacto');
+      // });
+      return $contact->$name;
     }
 }
