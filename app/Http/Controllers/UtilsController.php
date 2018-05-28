2caafd064d2a4697c12bc96978596a2375552c68
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2018-05-27 19:30:39 -0500

    App base

---
+++ app/Http/Controllers/UtilsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UtilsController extends Controller
+{
+    function contact (Request $request) {
+      return $request;
+    }
+}
